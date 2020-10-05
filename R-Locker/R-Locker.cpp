752cb92b371d0a100acfa216fc29b1fe5f33a389
Raúl Sánchez <raulsf66@gmail.com>
2020-10-05 17:50:40 +0200

    Added logger and black list

--- R-Locker/R-Locker.cpp
+++ R-Locker/R-Locker.cpp
@@ -1,17 +1,21 @@
 #include <windows.h>
 #include <string>
 #include <iostream>
+#include <fstream>
 #include <vector>
 #include <random>
 #include <time.h>
 #include <map>
 #include <vector>
+#include <chrono> 
 
 #define BUFSIZE 512
 #define MAXTHREADS 24
 
 std::vector<std::wstring> trap_paths;
 std::map<std::wstring, bool> whiteList;
+std::map<std::wstring, bool> blackList;
+std::wofstream logstream;
 std::wstring trap_target(L"\\\\.\\pipe\\trap");
 
 // Debug Function
@@ -30,6 +34,26 @@ std::string GetLastErrorAsString() {
     LocalFree(messageBuffer);
 
     return message;
+}
+
+
+std::wstring GetTimeStamp() {
+    SYSTEMTIME st;
+    GetSystemTime(&st);
+
+    return L"[" 
+        + std::to_wstring(st.wHour) + L":" 
+        + std::to_wstring(st.wMinute) + L":" 
+        + std::to_wstring(st.wSecond) 
+        + L"]";
+}
+
+void InitializeLog() {
+    SYSTEMTIME st;
+    GetSystemTime(&st);
+
+    logstream.open(std::to_wstring(st.wDay) + + L"." + std::to_wstring(st.wMonth) + L"." + std::to_wstring(st.wYear) + L".txt");
+    logstream << GetTimeStamp() << " " << L"Log Initialized" << std::endl;
 }
 
 
@@ -58,41 +82,55 @@ DWORD WINAPI NotifyUser(LPVOID param) {
     std::wstring program_name(buffer);
 
     if (!whiteList[program_name]) {
-        // Create message box text
-        std::wstring message(L"Process connected to pipe with PID ");
-        message += std::to_wstring(*ppid);
-        message += L" and name ";
-        message += program_name;
-        message += L". Do you want to stop it?";
+        if (!blackList[program_name]) {
+            // Create message box text
+            std::wstring message(L"Process connected to pipe with PID ");
+            message += std::to_wstring(*ppid);
+            message += L" and name ";
+            message += program_name;
+            message += L". Do you want to stop it?";
 
-        // Launch message box and wait until user responds
-        int msgboxID = MessageBox(
-            NULL,
-            std::wstring(message).c_str(),
-            std::wstring(L"R-Locker").c_str(),
-            MB_ICONWARNING | MB_YESNO | MB_DEFBUTTON1);
+            // Launch message box and wait until user responds
+            int msgboxID = MessageBox(
+                NULL,
+                std::wstring(message).c_str(),
+                std::wstring(L"R-Locker").c_str(),
+                MB_ICONWARNING | MB_YESNO | MB_DEFBUTTON1);
 
 
-        switch (msgboxID)
-        {
-        case IDYES:
-            // Kill process and close pipe handler
+            switch (msgboxID)
+            {
+            case IDYES:
+                // Kill process and close pipe handler
+                HANDLE handy;
+                handy = OpenProcess(SYNCHRONIZE | PROCESS_TERMINATE, TRUE, *ppid);
+                TerminateProcess(handy, 0);
+
+                blackList[program_name] = true;
+
+                std::wcout << " Terminated process and added to black list" << "\n";
+                logstream << GetTimeStamp() << " " << program_name <<  " was terminated and added to black list" << std::endl;
+
+                break;
+
+            case IDNO:
+                // Add process to the white list
+                whiteList[program_name] = true;
+                logstream << GetTimeStamp() << " " << program_name << " was added to white list" << std::endl;
+                break;
+            }
+        }
+        else {
             HANDLE handy;
             handy = OpenProcess(SYNCHRONIZE | PROCESS_TERMINATE, TRUE, *ppid);
             TerminateProcess(handy, 0);
 
-            std::wcout << "Terminated process" << "\n";
-
-            break;
-
-        case IDNO:
-            // Add process to the white list
-            whiteList[program_name] = true;
-            break;
+            logstream << GetTimeStamp() << " Program in the black list \"" << program_name << "\" tried to access the trap" << std::endl;
         }
+        
     }
     else {
-        std::wcout << "Program in the white list " << program_name <<  " tried to access the trap" << "\n";
+        logstream << GetTimeStamp() << " Program in the white list \"" << program_name <<  "\" tried to access the trap" << std::endl;
     }
 
     // Close handler
@@ -168,10 +206,13 @@ void WalkDirs(std::wstring dir_name) {
     WIN32_FIND_DATA data;
     std::wstring trap_name(dir_name + L"\\a.a");
 
-    // Create trap
-    CreateSymbolicLink(trap_name.c_str(), trap_target.c_str(), 0x0);
-    SetFileAttributes(trap_name.c_str(), FILE_ATTRIBUTE_HIDDEN);
-    trap_paths.push_back(trap_name);
+    // Create trap if it does not exist
+    HANDLE hTrap = FindFirstFileW(trap_name.c_str(), &data);
+    if (hTrap == INVALID_HANDLE_VALUE) {
+        CreateSymbolicLink(trap_name.c_str(), trap_target.c_str(), 0x0);
+        SetFileAttributes(trap_name.c_str(), FILE_ATTRIBUTE_HIDDEN);
+        trap_paths.push_back(trap_name);
+    }
 
     // Walk dirs
     std::wstring full_dir(dir_name + L"\\*");
@@ -248,9 +289,12 @@ void PopulateTraps() {
 }
 
 void PopulateTraps(PVOID lpParameter, BOOLEAN TimerOrWaitFired) {
+    auto start_traps = std::chrono::high_resolution_clock::now();
     PopulateTraps();
+    auto end_traps = std::chrono::high_resolution_clock::now();
+    double time_taken_traps = std::chrono::duration_cast<std::chrono::nanoseconds>(end_traps - start_traps).count();
+    std::wcout << "Elapsed time for periodic creation of traps: " << time_taken_traps * 1e-09 << " seconds" << "\n";
 }
-
 
 // Enum directories with benign .exe files and populate white list
 bool EnumDirs(std::vector<std::wstring> roots) {
@@ -297,6 +341,8 @@ BOOL WINAPI CtrlHandler(DWORD fdwCtrlType) {
         DeleteFile((*it).c_str());
     }
 
+    logstream.close();
+
     return TRUE;
 }
 
@@ -316,15 +362,25 @@ int wmain() {
     HANDLE hPipe = INVALID_HANDLE_VALUE, hThread = NULL;
     HANDLE  hThreadArray[MAXTHREADS];
 
+    InitializeLog();
+
     // Spread traps
+    auto start_traps = std::chrono::high_resolution_clock::now();
     PopulateTraps();
+    auto end_traps = std::chrono::high_resolution_clock::now();
+    double time_taken_traps = std::chrono::duration_cast<std::chrono::nanoseconds>(end_traps - start_traps).count();
+    std::wcout << "Elapsed time for initial creation of traps: " << time_taken_traps * 1e-09 << " seconds" << "\n";
 
     // Populate white list
     std::vector<std::wstring> roots;
     std::wstring mainDrive(GetMainDrive());
     roots.push_back(mainDrive + L"\\Program Files");
     roots.push_back(mainDrive + L"\\Program Files (x86)");
+    auto start_white = std::chrono::high_resolution_clock::now();
     EnumDirs(roots);
+    auto end_white = std::chrono::high_resolution_clock::now();
+    double time_taken_white = std::chrono::duration_cast<std::chrono::nanoseconds>(end_white - start_white).count();
+    std::wcout << "Elapsed time for white list population: " << time_taken_white * 1e-09 << " seconds" << "\n";
 
     // Get number of cores
     SYSTEM_INFO sys_info;
